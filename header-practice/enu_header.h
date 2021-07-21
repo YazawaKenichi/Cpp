329b1c932718cfaf34642d26c1d91008800a5d16
Yazawa <37500115+YazawaKenichi@users.noreply.github.com>
2021-07-21 19:26:46 +0900

    Update enu_header.h

--- header-practice/enu_header.h
+++ header-practice/enu_header.h
@@ -1,21 +1,15 @@
 #ifndef ENU_HEADER_H    //ENU_HEADER_H が定義されていないとき
 #define ENU_HEADER_H    //ENU_HEADER_H を定義する
 
-enum adrobo //未ロボ学生の列挙体を定義する
+enum Pref_ //未ロボ学生の列挙体を定義する
 {
-    ODA,
-    ONO,
-    KASAI,
-    KASHIWAGI,
-    KACHI,
-    KATSUKI,
-    KARIKOMI,
-    KAWAKAMI,
-    KAWAHARA,
-    KAWABATA,
-    KANNO,
-    KIMURA,
-    KUMAGAI
-} adrobo;
+    TOKYO,
+    KANAGAWA,
+    CHIBA,
+    SAITAMA,
+    GUNMA,
+    TOCHIGI,
+    IBARAKI
+} Pref;
 
 #endif  //#ifndef ENU_HEADER_H を抜ける
